3a2544cf1d63fc78f4ba8097e72b910f2f2e975c
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-10-11 09:21:10 +0200

    get set e attributi

--- pratica3.1-24/src/squadra/Giocatore.java
+++ pratica3.1-24/src/squadra/Giocatore.java
@@ -1,5 +1,21 @@
 package squadra;
 
 public class Giocatore {
+    private String nome;
+    private boolean capitano;
+    private int goal;
 
+    //Metodi get e set
+    public int getGoal() { return goal; }
+    public String getNome() { return nome; }
+    public boolean isCapitano() { return capitano; }
+
+    public void setCapitano(boolean capitano) { this.capitano = capitano; }
+    public void setGoal(int goal) {
+        if(goal<0)
+            this.goal = 0;
+        else
+            this.goal=goal;
+    }
+    public void setNome(String nome) { this.nome = nome; }
 }
